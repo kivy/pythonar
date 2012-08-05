8bcc33727d385e6f2a84aa1bcc8d91d0ee54e64f
gabriel.pettier <gabriel.pettier@gmail.com>
2012-08-05 22:07:40 +0200

    add -f --focus argument
    Allows to restore focus for N seconds after restarting application

--- reloader.py
+++ reloader.py
@@ -1,9 +1,9 @@
 #!/usr/bin/env python
 from watchdog.observers import Observer
 from watchdog.events import FileSystemEventHandler
-from subprocess import Popen
+from subprocess import Popen, PIPE
 from argparse import ArgumentParser, REMAINDER
-from time import sleep
+from time import time, sleep
 import re
 
 parser = ArgumentParser(description='''
@@ -18,14 +18,16 @@ Or any other use, the sky is the limit :)
 parser.add_argument('-p', '--path', type=str, default='.', help='set the path to monitor for changes')
 parser.add_argument('-a', '--action', type=str, default='restart', help='what action to perform when changes are detected')
 parser.add_argument('-i', '--ignorelist', type=str, default='', nargs='*', help='files to ignore')
+parser.add_argument('-f', '--focus', type=int, default=0, help='save focus and restore it for the next n seconds after restarting application, require xdotool (linux only)')
 parser.add_argument('command', type=str, nargs=REMAINDER)
 
 
 class RestartHandler(FileSystemEventHandler):
-    def __init__(self, command, path, ignorelist, **kwargs):
+    def __init__(self, command, path, ignorelist, focus, **kwargs):
         super(RestartHandler, self).__init__(**kwargs)
         self.command = command
         self.ignorelist = ignorelist
+        self.focus = focus
         self.start()
 
     def stop(self):
@@ -33,8 +35,17 @@ class RestartHandler(FileSystemEventHandler):
         print 'TERMINATED'
 
     def start(self):
+        if self.focus:
+            t = time()
+            wid = int(Popen(['/usr/bin/env', 'xdotool', 'getwindowfocus'], stdout=PIPE).communicate()[0])
+
         self._process = Popen(self.command)
         print 'STARTED', self._process
+
+        if self.focus:
+            while time() < t + self.focus:
+                Popen(['/usr/bin/env', 'xdotool', 'windowfocus', str(wid)])
+                sleep(.01)
 
     def on_any_event(self, event):
         for i in self.ignorelist:
@@ -46,9 +57,9 @@ class RestartHandler(FileSystemEventHandler):
         self.start()
 
 
-def monitor(command, path, action, ignorelist=None):
+def monitor(command, path, action, focus, ignorelist=None):
         if action == 'restart':
-            ev = RestartHandler(command, path=path, ignorelist=ignorelist)
+            ev = RestartHandler(command, path=path, focus=focus, ignorelist=ignorelist)
         else:
             raise NotImplementedError('action %s not implemented' % action)
 
@@ -65,4 +76,4 @@ def monitor(command, path, action, ignorelist=None):
 if __name__ == '__main__':
     args = parser.parse_args()
     print args
-    monitor(args.command, args.path, args.action, args.ignorelist)
+    monitor(args.command, args.path, args.action, args.focus, args.ignorelist)
