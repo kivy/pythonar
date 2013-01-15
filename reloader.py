e66ee3082d0f0e1f78ae6bbc9420947f3a55e65d
tshirtman <gabriel.pettier@gmail.com>
2013-01-15 15:21:49 +0100

    use xdotools to save/restore size/pos of window + flake8 fixes

--- reloader.py
+++ reloader.py
@@ -26,11 +26,18 @@ feedback.
 Or to restart a daemon when configuration or data changes.
 Or any other use, the sky is the limit :)
 ''')
-parser.add_argument('-p', '--path', type=str, default='.', help='set the path to monitor for changes')
-parser.add_argument('-a', '--action', type=str, default='restart', help='what action to perform when changes are detected')
-parser.add_argument('-i', '--ignorelist', type=str, default='', nargs='*', help='files to ignore')
-parser.add_argument('-f', '--focus', type=int, default=0, help='save focus and restore it for the next n seconds after restarting application, require xdotool (linux only)')
-parser.add_argument('-s', '--sleep', type=int, default=0, help='ignore events for n seconds after the last restart')
+parser.add_argument('-p', '--path', type=str, default='.',
+                    help='set the path to monitor for changes')
+parser.add_argument('-a', '--action', type=str, default='restart',
+                    help='what action to perform when changes are detected')
+parser.add_argument('-i', '--ignorelist', type=str, default='', nargs='*',
+                    help='files to ignore')
+parser.add_argument('-f', '--focus', type=int, default=0,
+                    help='save focus and restore it for the next n'
+                    'seconds after restarting application, require'
+                    'xdotool (linux only)')
+parser.add_argument('-s', '--sleep', type=int, default=0,
+                    help='ignore events for n seconds after the last restart')
 parser.add_argument('command', type=str, nargs=REMAINDER)
 
 
@@ -54,19 +61,42 @@ class RestartHandler(FileSystemEventHandler):
         self._process.terminate()
         log('red', 'TERMINATED')
 
-    def start(self):
+    def start(self, geometry=None):
         self._last_restart = time()
         if self.focus:
             t = time()
-            wid = int(Popen(['/usr/bin/env', 'xdotool', 'getwindowfocus'], stdout=PIPE).communicate()[0])
+            wid = int(Popen(['/usr/bin/env', 'xdotool', 'getwindowfocus'],
+                            stdout=PIPE).communicate()[0])
 
         self._process = Popen(self.command)
         log('green', 'STARTED %s' % self._process)
 
+        self.swid = int(Popen(['/usr/bin/env', 'xdotool', 'getwindowfocus'],
+                              stdout=PIPE).communicate()[0])
         if self.focus:
             while time() < t + self.focus:
+                # try to detect a window appeared by tracking focus
+                # change, NOT 100% safe!
+                swid = int(Popen(['/usr/bin/env', 'xdotool', 'getwindowfocus'],
+                                 stdout=PIPE).communicate()[0])
+                if swid != wid:
+                    print "got swid", swid
+                    self.swid = swid
+                    if geometry:
+                        self.replace(geometry)
+
                 Popen(['/usr/bin/env', 'xdotool', 'windowfocus', str(wid)])
                 sleep(.01)
+
+        if geometry:
+            self.replace(geometry)
+
+    def replace(self, geometry):
+        print geometry
+        x, y = geometry.split('\n')[1].strip().split(' ')[1].split(',')
+        w, h = geometry.split('\n')[2].strip().split(' ')[1].split('x')
+        Popen(['/usr/bin/env', 'xdotool', 'windowsize', str(self.swid), w, h])
+        Popen(['/usr/bin/env', 'xdotool', 'windowmove', str(self.swid), x, y])
 
     def on_any_event(self, event):
         if self.sleep and time() < self._last_restart + self.sleep:
@@ -81,13 +111,20 @@ class RestartHandler(FileSystemEventHandler):
 
         log('blue', '%s RESTARTING' % event)
 
+        geometry = None
+        if self.swid:
+            geometry = Popen(['/usr/bin/env', 'xdotool', 'getwindowgeometry',
+                              str(self.swid)],
+                             stdout=PIPE).communicate()[0]
         self.stop()
-        self.start()
+        self.start(geometry)
 
 
 def monitor(command, path, action, focus, sleeptime, ignorelist=None):
         if action == 'restart':
-            ev = RestartHandler(command, path=path, focus=focus, sleeptime=sleeptime, ignorelist=ignorelist)
+            ev = RestartHandler(command, path=path, focus=focus,
+                                sleeptime=sleeptime,
+                                ignorelist=ignorelist)
         else:
             raise NotImplementedError('action %s not implemented' % action)
 
@@ -104,4 +141,5 @@ def monitor(command, path, action, focus, sleeptime, ignorelist=None):
 if __name__ == '__main__':
     args = parser.parse_args()
     log('blue', str(args))
-    monitor(args.command, args.path, args.action, args.focus, args.sleep, args.ignorelist)
+    monitor(args.command, args.path, args.action, args.focus, args.sleep,
+            args.ignorelist)
